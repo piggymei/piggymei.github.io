af2c43597223432b1822643b6897cd8e51d9bd66
Jane <yumeij.2010@gmail.com>
2020-07-15 12:34:00 -0700

    rearragne files

--- script.js
+++ script.js
@@ -1,86 +1,176 @@
-//When the "create keyword" button is clicked, a card with:
-document.querySelector("#search_button").addEventListener("click", createCard);
-let searchKey;
+document.getElementById("logout").addEventListener("click", logoutclick);
 
-function appendContents(content, type) {
-  let card = document.createElement("div");
-  card.setAttribute("class", "card-body");
-  let title = document.createElement("h5");
-  title.setAttribute("class", "card-title");
-  debugger;
+function logoutclick() {
+  location.href = "login.html";
+}
 
+document.querySelector("form").addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  var wishListContainer = document.querySelector("#wish_container");
+  // Change wishlist title if the wishlist was empty
+  if (wishListContainer.children.length === 0) {
+    document.querySelector("#title").innerHTML = "My WishList";
+  }
+  let des1 = document.querySelector("#dest").innerText;
+  let loc = document.querySelector("#loc").innerText;
+  let pho = document.querySelector("#pho").innerText;
+  let desc = document.querySelector("#desc").value;
+  let card_div = createCard(des1, loc, pho, desc);
+  document.querySelector("#wish_container").append(card_div);
+  event.target.elements[0].value = "";
+  event.target.elements[1].value = "";
+  event.target.elements[2].value = "";
+  event.target.elements[3].value = "";
+}
+
+function createCard(des1, lo, ph, des2) {
+  // let card = document.createElement("div");
+  // card.className = "card";
+  // let title = document.createElement("h5");
+  // title.innerText = des1;
+  // title.setAttribute("class", "card-title");
+  // card.appendChild(title);
+  // document.querySelector("#card_container").append(card);
+
+  let desc = des2;
+  let loc = lo;
+  let pho = ph;
+  let dest = des1;
+
+  let div1 = document.createElement("div");
+  let div2 = document.createElement("div");
   let img = document.createElement("img");
+  let head5 = document.createElement("h5");
+  let para = document.createElement("p");
+  let para2 = document.createElement("p");
+  let button1 = document.createElement("button");
+  let button2 = document.createElement("button");
+  var buttonsContainer = document.createElement("div");
+  para.textContent = desc;
+  para2.textContent = loc;
+  head5.textContent = dest;
+  img.src = pho;
+
+  if (pho.length === 0 || !pho.includes("http")) {
+    img.src =
+      "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg";
+  }
+
+  div1.setAttribute("class", "card");
+  div2.setAttribute("class", "class-body");
+  head5.setAttribute("class", "card-title");
   img.setAttribute("class", "card-img-top");
-  console.log(content);
-  if (type === "mov") {
-    title.innerHTML = content.Title;
-    img.src = content.Poster;
-  } else {
-    title.innerHTML = content.title;
-    img.src = content.images.original.url;
-  }
-  card.append(title, img);
-  document.querySelector("#final_results").append(card);
+  img.setAttribute("alt", dest);
+  para.setAttribute("class", "card-text");
+  para2.setAttribute("class", "card-text");
+
+  div1.style.width = "15rem";
+  div1.style.height = "fit-content";
+  div1.style.margin = "20px";
+  button1.textContent = "Edit";
+  button1.style.backgroundColor = "yellow";
+  button2.addEventListener("click", removeDestination);
+  button1.addEventListener("click", edit);
+
+  button2.textContent = "Remove";
+  button2.style.backgroundColor = "pink";
+  buttonsContainer.setAttribute("class", "buttons_container");
+
+  div2.appendChild(head5);
+  div2.appendChild(para2);
+  div2.appendChild(para);
+  buttonsContainer.appendChild(button1);
+  buttonsContainer.appendChild(button2);
+  div2.appendChild(buttonsContainer);
+
+  div1.appendChild(img);
+  div1.appendChild(div2);
+  return div1;
+  document.querySelector("#card_container").append(div1);
 }
 
-function getContents(e) {
-  const id = e.target.id;
-  //choose URL depending on id //KzW6J0DbGeAl45Wl0Np6SZPJalttCYIv  "http://www.omdbapi.com/?&apikey=a92d1656&s=";
-  let url;
-  if (id === "movie_btn") {
-    url = "http://www.omdbapi.com/?apikey=a92d1656&s=";
-  } else {
-    url =
-      "http://api.giphy.com/v1/gifs/search?api_key=KzW6J0DbGeAl45Wl0Np6SZPJalttCYIv&limit=12&q=";
-  }
-  url = url.concat(searchKey);
+function edit(event) {
+  var cardBody = event.target.parentElement.parentElement;
+  var title = cardBody.children[0];
+  var subTitle = cardBody.children[1];
+  var loc = cardBody.children[2];
+  var card = cardBody.parentElement;
+  var photoUrl = card.children[0];
 
-  fetch(url)
+  var newTitle = window.prompt("Enter new name");
+  var newSubtitle = window.prompt("Enter new location");
+  var newPhotoUrl = window.prompt("Enter new photo url");
+  var newdesc = window.prompt("Enter new description");
+  if (newTitle.length > 0) {
+    title.innerText = newTitle;
+  }
+
+  if (newSubtitle.length > 0) {
+    subTitle.innerText = newSubtitle;
+  }
+
+  if (newPhotoUrl.length > 0) {
+    photoUrl.setAttribute("src", newPhotoUrl);
+  }
+
+  if (newdesc.length > 0) {
+    loc.innerText = newdesc;
+  }
+}
+
+function removeDestination(event) {
+  var cardBody = event.target.parentElement.parentElement;
+  var card = cardBody.parentElement;
+  card.remove();
+}
+
+const clickButton = (e) => {
+  let searchKeyword = e.target.innerHTML;
+  let sUrl = "http://www.omdbapi.com/?&apikey=a92d1656&s=";
+  let searchUrl = sUrl.concat(searchKeyword);
+
+  fetch(searchUrl)
     .then((response) => response.json())
-    .then((data) => {
-      let content, type;
-      if (id === "movie_btn") {
-        content = data.Search;
-        type = "mov";
-      } else {
-        content = data.data;
-        type = "gif";
-      }
-      content.forEach((element) => {
-        appendContents(element, type);
-      });
-    });
-}
+    .then((data) => processData(data.Search));
+};
 
-//create div.card
-function createCard(e) {
+document.getElementById("submit_btn").addEventListener("click", (e) => {
   e.preventDefault();
-  let card = document.createElement("div");
-  card.setAttribute("class", "card-body");
 
-  //add keyword as a title
-  searchKey = document.querySelector("#search").value;
-  let title = document.createElement("h5");
-  title.className = "card-title";
-  title.innerText = searchKey;
-  card.append(title);
+  let searchKeyword = document.getElementById("search").value;
 
-  //create 2 buttons:
-  //One button to search for at most 12 movies that have that keyword in their title
-  let movieBtn = document.createElement("button");
-  movieBtn.setAttribute("id", "movie_btn");
-  movieBtn.className = "btn btn-primary";
-  movieBtn.innerText = "movie";
-  movieBtn.addEventListener("click", getContents);
+  let btn = document.createElement("button");
 
-  //One button to search for at most 12 gifs about that keyword
-  let gifBtn = document.createElement("button");
-  gifBtn.setAttribute("id", "gif_btn");
-  gifBtn.className = "btn btn-primary";
-  gifBtn.innerText = "gif";
-  gifBtn.addEventListener("click", getContents);
+  btn.innerHTML = searchKeyword;
 
-  card.append(movieBtn, gifBtn);
+  document.getElementById("search").value = "";
 
-  document.querySelector("#search_results").append(card);
+  document.getElementById("search_btn_container").appendChild(btn);
+
+  btn.addEventListener("click", clickButton);
+});
+function processData(movieList) {
+  //console.log(movieList);
+  movieList.forEach((element) => {
+    console.log(element);
+    let div1 = createCard(
+      element.Title,
+      element.Year,
+      element.Poster,
+      element.Type
+    );
+    document.querySelector("#card_container").append(div1);
+  });
 }
+
+// function clickButton(e) {
+//   let searchKeyword = e.innerHTML;
+//   let sUrl = "http://www.omdbapi.com/?&apikey=a92d1656&s=";
+//   let searchUrl = sUrl.concat(searchKeyword);
+
+//   fetch(searchUrl)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.Search));
+// }
